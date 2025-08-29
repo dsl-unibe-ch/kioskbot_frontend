d5ab3e1bcbb4c09b16584d281fd1ae43c4fdda04
Sebi <sebastian@flick.solutions>
2025-08-29 13:15:54 +0200

    change name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -189,7 +189,7 @@
 <h1
 	class="sticky top-0 z-10 mb-9 scroll-m-20 bg-white pb-4 text-center text-3xl font-extrabold tracking-tight text-balance lg:text-5xl"
 >
-	Informationskioskbot der Universität Bern
+	Informationskiosk der Universität Bern
 	<p class="mt-2 text-lg font-medium">
 		Der Bot beantwortet Fragen rund um die Abteilung für Qualitätssicherung und -entwicklung.
 	</p>
